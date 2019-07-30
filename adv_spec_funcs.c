3e21063936f8321f2822f0e25318de9d76efd490
Jose Alvarez de Lugo <845@holbertonschool.com>
2019-07-29 20:38:27 -0400

    add reverse function & malloc space for rot13

--- adv_spec_funcs.c
+++ adv_spec_funcs.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+#include <stdlib.h>
 
 /**
  * rot13_find - encodes a string using rot13
@@ -8,19 +9,40 @@
 
 char *rot13_find(va_list s)
 {
-	int i, x, temp;
+	int i, x, temp, len;
 	char check[] = "AaZz";
 	char ntom[] = "NOPQRSTUVWXYZABCDEFGHIJKLM";
-	char *arg;
+	char *arg, *rot;
 
 	arg = va_arg(s, char *);
-
-	for (i = 0; arg[i] != '\0'; i++)
+	len = _strlen(arg);
+	rot = malloc((len + 1) * sizeof(char));
+	for (i = 0; arg[i]; i++)
 		for (x = 0; x < 2; x++)
 			if (arg[i] >= check[x] && arg[i] <= check[x + 2])
 			{
 				temp = (arg[i] - 65 - (x * 32));
-				arg[i] = (ntom[temp] + (x * 32));
+				rot[i] = (ntom[temp] + (x * 32));
 			}
-	return (arg);
+	rot[i] = '\0';
+	return (rot);
+}
+/**
+ * rev_find - reverse a string
+ * @s: input string
+ * Return: reversed string
+ */
+char *rev_find(va_list s)
+{
+	char *arg, *rev;
+	int i, len;
+
+	arg = va_arg(s, char *);
+	len = _strlen(arg);
+	rev = malloc((len + 1) * sizeof(char));
+
+	len--;
+	for (i =0; len >= 0; i++, len--)
+		rev[i] = arg[len];
+	return (rev);
 }
